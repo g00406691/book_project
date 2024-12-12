7f531d4237dfa999bfd1351cdc9fb0c55bd86110
unknown <g00406691@atu.ie>
2024-12-12 23:10:52 +0000

    styling changes to the navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,21 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import '../App.css'; // Ensure we import the main CSS file for custom classes
 
 const NavigationBar = () => {
   return (
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="/">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/create">Create</Nav.Link>
-              <Nav.Link href="/read">Read</Nav.Link>
-            </Nav>
-          </Container>
-      </Navbar>
+    <Navbar bg="dark" data-bs-theme="dark" className="book-navbar">
+      <Container>
+        <Navbar.Brand href="/" className="book-navbar-brand">
+          My Book App
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/" className="book-nav-link">Home</Nav.Link>
+          <Nav.Link href="/create" className="book-nav-link">Add Book</Nav.Link>
+          <Nav.Link href="/read" className="book-nav-link">View Books</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
